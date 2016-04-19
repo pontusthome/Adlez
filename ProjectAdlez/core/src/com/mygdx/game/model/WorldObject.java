eded78644beec4ef9795771a574934687f0da556
Martin So <martin.so@live.com>
2016-04-19 10:14:14 +0200

    implemented WorldObject

---
+++ ProjectAdlez/core/src/com/mygdx/game/model/WorldObject.java
@@ -0,0 +1,24 @@
+package com.mygdx.game.model;
+
+public abstract class WorldObject {
+
+    private float posX;
+    private float posY;
+
+    public float getPosX() {
+        return posX;
+    }
+
+    public void setPosX(float x) {
+        this.posX = x;
+    }
+
+    public float getPosY() {
+        return posX;
+    }
+
+    public void setPosY(float y) {
+        this.posY = y;
+    }
+
+}
