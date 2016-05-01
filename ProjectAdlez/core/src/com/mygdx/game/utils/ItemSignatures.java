b8cbdf7d1e9902d413c743e67cb8aa26e57c8f59
Martin So <martin.so@live.com>
2016-05-01 14:25:30 +0200

    complete items with stats implemented and accessible from utils

---
+++ ProjectAdlez/core/src/com/mygdx/game/utils/ItemSignatures.java
@@ -0,0 +1,36 @@
+package com.mygdx.game.utils;
+
+/**
+ * Created by martinso on 01/05/16.
+ */
+
+/**
+ * final class holding all item-signatures.
+ */
+
+public final class ItemSignatures {
+
+    /**
+     * Types
+     */
+    public static final String TYPE_WEAPON_SWORD = "type_weapon_sword";
+
+    public static final String TYPE_ARMOR_SHIELD = "type_armor_shield";
+    public static final String TYPE_ARMOR_BODY = "type_armor_body";
+    public static final String TYPE_ARMOR_HEAD = "type_armor_head";
+
+    /**
+     * Swords
+     */
+    public static final String WOODEN_SWORD = "wooden_sword";
+    public static final String IRON_SWORD = "iron_sword";
+    public static final String FINAL_SWORD = "final_sword";
+
+    /**
+     * Armor
+     */
+    public static final String WOODEN_BODY_ARMOR = "wooden_body_armor";
+
+    // add more...
+
+}
