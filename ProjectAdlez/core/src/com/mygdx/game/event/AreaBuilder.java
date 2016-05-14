eeeda60dec17a877ad1db36a1ea38edaebe511a0
Pontus Thomé <pontusthome@gmail.com>
2016-05-14 07:42:10 +0200

    added a AreaBuilder and AreaIO

---
+++ ProjectAdlez/core/src/com/mygdx/game/event/AreaBuilder.java
@@ -0,0 +1,40 @@
+package com.mygdx.game.event;
+
+import java.io.FileWriter;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+/**
+ * Created by Pontus on 2016-05-14.
+ */
+public class AreaBuilder implements AreaIO {
+
+    @Override
+    public void savePlayer() {
+
+    }
+
+    @Override
+    public void saveEnemies() {
+            JSONObject obj = new JSONObject();
+            obj.put("Name", "crunchify.com");
+            obj.put("Author", "App Shah");
+
+            JSONArray company = new JSONArray();
+            company.add("Compnay: eBay");
+            company.add("Compnay: Paypal");
+            company.add("Compnay: Google");
+            obj.put("Company List", company);
+
+            // try-with-resources statement based on post comment below :)
+            try (FileWriter file = new FileWriter("enemies.txt")) {
+                file.write(obj.toJSONString());
+                System.out.println("Successfully Copied JSON Object to File...");
+                System.out.println("\nJSON Object: " + obj);
+            }
+    }
+}
---
+++ ProjectAdlez/core/src/com/mygdx/game/event/AreaIO.java
@@ -0,0 +1,9 @@
+package com.mygdx.game.event;
+
+/**
+ * Created by Pontus on 2016-05-14.
+ */
+public interface AreaIO {
+    public void savePlayer();
+    public void saveEnemies();
+}
